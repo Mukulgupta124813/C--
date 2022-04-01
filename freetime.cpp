7affc96cbf227ea6cb35b0d8a81be15f1a52b87e
Mukulgupta124813 <mymukpta420@gmail.com>
2022-04-01 09:26:37 +0530

    add

---
+++ freetime.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  int m,n;
+  cin>>n>>m;
+  int arr1[n],arr2[m];
+  for(int i=0;i<n;i++){
+      cin>>arr1[i];
+  }
+  for(int i=0;i<m;i++){
+      cin>>arr2[i];
+  }
+  
+    return 0;
+}
